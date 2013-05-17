fd013a74ab4d7840e259efe82764eb475505897e
Uwe Schmelzer <us@dvschmelzer.de>
2013-05-17 12:28:04 +0200

    Commit only

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+	/**
+	 * Created by JetBrains PhpStorm.
+	 * User: schmelzer
+	 * Date: 17.05.13
+	 * Time: 12:26
+	 * To change this template use File | Settings | File Templates.
+	 */
+	echo phpinfo();
